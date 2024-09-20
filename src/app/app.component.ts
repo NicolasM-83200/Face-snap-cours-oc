8ae750d6c7eb4508dd244f938d7fe5abfedc3c01
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-09-20 10:13:38 +0200

    Afficher les données

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @Component({
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './face-snap.component.html',
-  styleUrl: './face-snap.component.scss'
+  styleUrl: './face-snap.component.scss',
 })
 export class FaceSnapComponent {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
+  ngOnInit(): void {
+    this.title = 'Chamalow';
+    this.description = 'Le gros veau qui miaule tout le temps';
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageUrl =
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
 }
