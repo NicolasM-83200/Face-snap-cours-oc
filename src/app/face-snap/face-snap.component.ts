3cfe39f01e3703c1c842ff0740b31bd8cfe1defd
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-09-23 13:52:34 +0200

    Mettre de la classe

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './face-snap.component.html',
 })
 export class FaceSnapComponent implements OnInit {
