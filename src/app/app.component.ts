28d535f804e3091c8f3878d0fc7bcd555208ee2c
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-09-23 11:21:30 +0200

    Afficher des listes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,33 +9,33 @@ import { FaceSnap } from './models/face-snap';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis toujours!',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      10
-    );
-    this.myOtherSnap = new FaceSnap(
-      'Poulpy',
-      'Le pire ennemi de mon meilleur ami!',
-      'https://boutique.plushtoy.fr/WebRoot/ce_fr/Shops/186372/5F84/B95E/589A/04C6/565D/C0A8/1911/2000/peluche-pieuvre-110-cm-orange_ml.jpg',
-      new Date(),
-      1
-    );
-    this.myLastSnap = new FaceSnap(
-      'Lapinou',
-      'Un lapin vachement fort!',
-      'https://gastonethortense.com/4329-large_default/product.jpg',
-      new Date(),
-      10
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        'Archibald',
+        'Mon meilleur ami depuis toujours!',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        10
+      ),
+      new FaceSnap(
+        'Poulpy',
+        'Le pire ennemi de mon meilleur ami!',
+        'https://boutique.plushtoy.fr/WebRoot/ce_fr/Shops/186372/5F84/B95E/589A/04C6/565D/C0A8/1911/2000/peluche-pieuvre-110-cm-orange_ml.jpg',
+        new Date(),
+        1
+      ),
+      new FaceSnap(
+        'Lapinou',
+        'Un lapin vachement fort!',
+        'https://gastonethortense.com/4329-large_default/product.jpg',
+        new Date(),
+        10
+      ),
+    ];
 
-    this.myOtherSnap.setLocation("Dans les profondeurs de l'océan");
+    this.faceSnaps[1].setLocation("Dans les profondeurs de l'océan");
   }
 }
