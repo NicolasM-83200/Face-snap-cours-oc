8ec8924d45d091d545cd796362574c96bd079457
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-09-26 15:12:11 +0200

    Passer en SPA avec le routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
-import { NgStyle } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FaceSnapListComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
